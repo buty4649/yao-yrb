faeb4c5b58ec0ad98fa7cfab356e2c18cc651ace
Yuki Koya <ykky@pepabo.com>
2021-12-10 14:45:37 +0900

    rubocop

--- lib/yao/yrb/cli.rb
+++ lib/yao/yrb/cli.rb
@@ -5,48 +5,53 @@ require 'irb'
 # [HACK] allow optional and multivalued parameters
 original_verbosity = $VERBOSE
 $VERBOSE = nil
-Clamp::Parameter::Definition::ELLIPSIS_SUFFIX = / \.\.\.\]?$/
+Clamp::Parameter::Definition::ELLIPSIS_SUFFIX = / \.\.\.\]?$/.freeze
 $VERBOSE = original_verbosity
 
-module Yao::Yrb
-  class Cli < Clamp::Command
-    option '--version', :flag, 'Show version' do
-      puts Yao::Yrb::VERSION
-      exit(0)
-    end
-
-    parameter '[FILE ...]', 'Execute the contents of FILE. If unset, run in Interpreter mode.',
-              attribute_name: :script_mode
-
-    def execute
-      Yao.configure do
-        auth_url              ENV['OS_AUTH_URL']
-        tenant_name           ENV['OS_TENANT_NAME']
-        username              ENV['OS_USERNAME']
-        password              ENV['OS_PASSWORD']
-        ca_cert               ENV['OS_CACERT']
-        client_cert           ENV['OS_CERT']
-        client_key            ENV['OS_KEY']
-        region_name           ENV['OS_REGION_NAME']
-        identity_api_version  ENV['OS_IDENTITY_API_VERSION']
-        user_domain_name      ENV['OS_USER_DOMAIN_NAME']
-        project_domain_name   ENV['OS_PROJECT_DOMAIN_NAME']
-        debug                 ENV['YAO_DEBUG']
-        debug_record_response ENV['YAO_DEBUG_RECORD_RESPONSE']
+module Yao
+  module Yrb
+    class Cli < Clamp::Command
+      option '--version', :flag, 'Show version' do
+        puts Yao::Yrb::VERSION
+        exit(0)
       end
 
-      if script_mode.size > 0
-        script_file = script_mode.first
-        load script_file
-      else
-        IRB.setup(__FILE__)
-        conf = IRB.conf
-        conf[:AP_NAME] = 'yrb'
-        conf[:IRB_NAME] = 'yrb'
-        irb = IRB::Irb.new
-        irb.run(conf)
+      parameter '[FILE ...]', 'Execute the contents of FILE. If unset, run in Interpreter mode.',
+                attribute_name: :script_mode
+
+      def execute
+        yao_setup
+
+        if script_mode.size
+          script_file = script_mode.first
+          load script_file
+        else
+          IRB.setup(__FILE__)
+          conf = IRB.conf
+          conf[:AP_NAME] = 'yrb'
+          conf[:IRB_NAME] = 'yrb'
+          irb = IRB::Irb.new
+          irb.run(conf)
+        end
       end
 
+      def yao_setup
+        Yao.configure do
+          auth_url              ENV['OS_AUTH_URL']
+          tenant_name           ENV['OS_TENANT_NAME']
+          username              ENV['OS_USERNAME']
+          password              ENV['OS_PASSWORD']
+          ca_cert               ENV['OS_CACERT']
+          client_cert           ENV['OS_CERT']
+          client_key            ENV['OS_KEY']
+          region_name           ENV['OS_REGION_NAME']
+          identity_api_version  ENV['OS_IDENTITY_API_VERSION']
+          user_domain_name      ENV['OS_USER_DOMAIN_NAME']
+          project_domain_name   ENV['OS_PROJECT_DOMAIN_NAME']
+          debug                 ENV['YAO_DEBUG']
+          debug_record_response ENV['YAO_DEBUG_RECORD_RESPONSE']
+        end
+      end
     end
   end
 end
--- yao-yrb.gemspec
+++ yao-yrb.gemspec
@@ -1,44 +1,48 @@
-
-lib = File.expand_path("../lib", __FILE__)
+lib = File.expand_path(__dir__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-require "yao/yrb/version"
+require 'yao/yrb/version'
 
 Gem::Specification.new do |spec|
-  spec.name          = "yao-yrb"
+  spec.name          = 'yao-yrb'
   spec.version       = Yao::Yrb::VERSION
-  spec.authors       = ["Yuki Koya"]
-  spec.email         = ["buty4649@gmail.com"]
+  spec.authors       = ['Yuki Koya']
+  spec.email         = ['buty4649@gmail.com']
 
-  spec.summary       = %q{irb with yao}
-  spec.description   = %q{irb with yao (https://github.com/yaocloud/yao)}
-  spec.homepage      = "https://github.com/buty4649/yao-yrb"
+  spec.summary       = 'irb with yao'
+  spec.description   = 'irb with yao (https://github.com/yaocloud/yao)'
+  spec.homepage      = 'https://github.com/buty4649/yao-yrb'
 
   # Prevent pushing this gem to RubyGems.org. To allow pushes either set the 'allowed_push_host'
   # to allow pushing to a single host or delete this section to allow pushing to any host.
   if spec.respond_to?(:metadata)
 
-    spec.metadata["homepage_uri"] = spec.homepage
-    spec.metadata["source_code_uri"] = "https://github.com/buty4649/yao-yrb"
-    spec.metadata["changelog_uri"] = "https://github.com/buty4649/yao-yrb"
+    spec.metadata['rubygems_mfa_required'] = 'true'
+    spec.metadata['homepage_uri'] = spec.homepage
+    spec.metadata['source_code_uri'] = 'https://github.com/buty4649/yao-yrb'
+    spec.metadata['changelog_uri'] = 'https://github.com/buty4649/yao-yrb'
   else
-    raise "RubyGems 2.0 or newer is required to protect against " \
-      "public gem pushes."
+    raise 'RubyGems 2.0 or newer is required to protect against ' \
+          'public gem pushes.'
   end
 
   # Specify which files should be added to the gem when it is released.
   # The `git ls-files -z` loads the files in the RubyGem that have been added into git.
-  spec.files         = Dir.chdir(File.expand_path('..', __FILE__)) do
+  spec.files = Dir.chdir(File.expand_path(__dir__)) do
     `git ls-files -z`.split("\x0").reject { |f| f.match(%r{^(test|spec|features)/}) }
   end
-  spec.bindir        = "exe"
+  spec.bindir        = 'exe'
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
-  spec.require_paths = ["lib"]
+  spec.require_paths = ['lib']
+
+  spec.required_ruby_version = '>= 2.7.0'
 
   spec.add_dependency 'yao'
+
   spec.add_dependency 'clamp', '~> 1.3.1'
   spec.add_dependency 'pry', '~> 0.13.1'
 
-  spec.add_development_dependency "bundler", "~> 2.0"
-  spec.add_development_dependency "rake", ">= 12.3.3"
-  spec.add_development_dependency "minitest", "~> 5.0"
+  spec.add_development_dependency 'bundler', '~> 2.0'
+  spec.add_development_dependency 'minitest', '~> 5.0'
+  spec.add_development_dependency 'rake', '>= 12.3.3'
+  spec.add_development_dependency 'rubocop'
 end
